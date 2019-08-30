0ee3276f8b79ca3a8c99ebfd69bea4f5d7ee1aec
方涛 <375305409@qq.com>
2019-08-30 17:40:53 +0800

    添加项目文件。

---
+++ RedisComm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisComm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisHelper.library = 1;
+            RedisHelper.GetHashList("2");
+        }
+    }
+}
---
+++ RedisComm/RedisHelper.cs
@@ -0,0 +1,69 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisComm
+{
+    public class RedisHelper
+    {
+        private static ConfigurationOptions configurationOptions = ConfigurationOptions.Parse("192.168.124.12:6379,password=123456,connectTimeout=2000");
+
+        //the lock for singleton
+        private static readonly object Locker = new object();
+
+        //singleton
+        private static ConnectionMultiplexer redisConn =ConnectionMultiplexer.Connect(configurationOptions);
+
+        public static int library = 0;
+        #region 连接redis
+        //singleton
+        public static ConnectionMultiplexer getRedisConn()
+        {
+
+            if (redisConn == null)
+            {
+                lock (Locker)
+                {
+                    if (redisConn == null || !redisConn.IsConnected)
+                    {
+                        redisConn = ConnectionMultiplexer.Connect(configurationOptions);
+                    }
+                }
+            }
+            return redisConn;
+        }
+        #endregion
+
+        public static string GetHashString(string  mainKey,string key)
+        {
+            var database = redisConn.GetDatabase(library);
+            string hash = database.HashGet(mainKey,key);
+            return hash;
+        }
+
+        public static void GetHashList(string mianKey)
+        {
+            Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+            var database = redisConn.GetDatabase(library);
+            var hash = database.HashGetAll(mianKey);
+            List<HashTables> hts = new List<HashTables>();
+            foreach (var item in hash)
+            {
+
+            }
+
+
+        }
+    }
+
+    public class HashTables
+    {
+        public string HashKey { get; set; }
+
+        public string HashValue { get; set; }
+    }
+}
